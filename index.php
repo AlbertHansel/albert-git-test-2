fb275ca362a161ed2ddfc36f682676449b75459e
AlbertHansel <alberthansel00@gmail.com>
2024-09-17 11:47:10 +0700

    Menambahkan Paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quaerat veritatis nisi quod rem dolore dolorum amet minus. Et ratione molestiae saepe porro, fuga laboriosam quas at quidem nihil aliquam?</p>
 </body>
 </html>
